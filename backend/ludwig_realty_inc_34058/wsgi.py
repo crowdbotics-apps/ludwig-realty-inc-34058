c719f2f93ead0e74e9397e6052fdc28e4a71cbd7
Crowdbotics <team@crowdbotics.com>
2022-03-08 20:08:48 +0000

    Initial commit.

---
+++ backend/ludwig_realty_inc_34058/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ludwig_realty_inc_34058 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludwig_realty_inc_34058.settings')
+
+application = get_wsgi_application()
